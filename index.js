69b4630f5e6306ca3f12b4fea0b5d617a0ada41a
Camille98 <cetienne@scmail.spelman.edu>
2020-06-02 21:49:21 -0400

    Add files via upload
    Spent too much time on webpack.js error

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Fusion from './fusion.js';
+import './fusion.html';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+        <Fusion />,
+    document.getElementById('root')
+);
+
+
